5f0ce213f1a749bc93020a57969f05667b8e5cc1
NatStopa <nat.stopa.nat@gmail.com>
2021-12-11 19:37:28 +0100

    Added player guess

--- guess-game.js
+++ guess-game.js
@@ -1,6 +1,11 @@
 function guessGame() {
   let randomNumber = Math.round(Math.random() * 10);
-  console.log(randomNumber);
+  let guess;
+  do {
+    guess = prompt("Guess the number from 0 to 10");
+    console.log(guess, randomNumber);
+  } while (guess != randomNumber);
+  alert("You won!");
 }
 
 guessGame();
