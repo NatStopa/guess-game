43da504fed692cb458a994528ac85f27695f9e07
NatStopa <nat.stopa.nat@gmail.com>
2021-12-11 19:41:15 +0100

    Added hints

--- guess-game.js
+++ guess-game.js
@@ -3,7 +3,11 @@ function guessGame() {
   let guess;
   do {
     guess = prompt("Guess the number from 0 to 10");
-    console.log(guess, randomNumber);
+    if (guess > randomNumber) {
+      alert("Your choose is too high, try again");
+    } else if (guess < randomNumber) {
+      alert("Your choose is too low, try again");
+    }
   } while (guess != randomNumber);
   alert("You won!");
 }
