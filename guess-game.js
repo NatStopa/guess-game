fcdd6a082b920f17b22a2cee33fea68d0cff01e7
NatStopa <nat.stopa.nat@gmail.com>
2022-02-15 12:44:44 +0100

    Made game functionality

--- guess-game.js
+++ guess-game.js
@@ -1,15 +1,25 @@
-function guessGame() {
-  let randomNumber = Math.round(Math.random() * 10);
-  let guess;
-  do {
-    guess = prompt("Guess the number from 0 to 10");
-    if (guess > randomNumber) {
-      alert("Your choose is too high, try again");
-    } else if (guess < randomNumber) {
-      alert("Your choose is too low, try again");
+function game() {
+  const chooseSubmit = document.querySelector("#choose-button");
+  const choiceInput = document.querySelector("#choose-input");
+  const userChoicePreview = document.querySelector("#user-choice-preview");
+  const computerChoicePreview = document.querySelector(
+    "#computer-choice-preview"
+  );
+  const result = document.querySelector("#result");
+  let computerChoice = Math.floor(Math.random() * 10) + 1;
+  chooseSubmit.onclick = function (event) {
+    event.preventDefault();
+    let userChoice = choiceInput.value;
+    userChoicePreview.innerHTML = userChoice;
+    if (computerChoice < userChoice) {
+      result.innerHTML = "Your choose is too high, try again";
+    } else if (computerChoice > userChoice) {
+      result.innerHTML = "Your choose is too low, try again";
+    } else {
+      computerChoicePreview.innerHTML = computerChoice;
+      result.innerHTML = "You won!";
     }
-  } while (guess != randomNumber);
-  alert("You won!");
+  };
 }
 
-guessGame();
+game();
